a37175cf82d879fbcab861c1c7a7cd7637fb98f4
kristykawai <kristy.kw.chan@gmail.com>
2024-02-09 13:32:51 +1030

    tested current time in ms

--- includes/philo.h
+++ includes/philo.h
@@ -2,5 +2,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/syscall.h>
+#include <time.h>
+#include <sys/time.h>
 
 #define PHILO_NUM 5
--- src/main.c
+++ src/main.c
@@ -1,8 +1,4 @@
 #include "philo.h"
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/syscall.h>
 
 int mails = 0;
 pthread_mutex_t mutex;
@@ -27,13 +23,31 @@ void	*routine()
 	return(0);
 }
 
+int	gettime()
+{
+	struct  timeval current_time;
+	time_t current_time_ms;
+
+	if(gettimeofday(&current_time, NULL) != 0)
+		return(-1);
+	current_time_ms = current_time.tv_sec * 1000 + current_time.tv_usec/1000;
+	// printf("millisecond: %ld\n",(long)current_time.tv_usec);
+	return(current_time_ms);
+}
+
 int main()
 {
 	pthread_t th[PHILO_NUM];
 	pthread_mutex_init(&mutex, NULL);
 	int	i;
+	time_t start_time;
+	time_t end_time;
+	time_t processed_time;
 
 	i = 0;
+	start_time = gettime(); 
+	if(start_time == -1)
+		perror("can't get current time");
 	while(i < PHILO_NUM)
 	{
 		if (pthread_create(&th[i], NULL, &routine, NULL) != 0)
@@ -47,5 +61,8 @@ int main()
 	}
 	pthread_mutex_destroy(&mutex);
 	printf("print mail num:%d\n", mails);
+	end_time = gettime();
+	processed_time = end_time - start_time;
+	printf("processed time in millisecond: %ld",(long)processed_time);
 	return (0);
 }
